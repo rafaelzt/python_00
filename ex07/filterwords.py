dc7f4b1028a029c3a83475164c0f6e82abbb4784
Rafael Z Tanganelli <kabelu@gmail.com>
2023-04-16 22:43:19 +0200

    refactoring filterwords

--- ex07/filterwords.py
+++ ex07/filterwords.py
@@ -6,40 +6,28 @@
 #    By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/12 17:23:49 by rzamolo-          #+#    #+#              #
-#    Updated: 2023/04/12 17:28:04 by rzamolo-         ###   ########.fr        #
+#    Updated: 2023/04/16 22:19:39 by rzamolo-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
+def ft_filterwords(*arguments):
+	lst = arguments
+	try:
+		_, word, length = lst[0]
+		try:
+			length = int(length)
+			print(length)
+		except:
+			print("Error: Second parameter must be a integer")
+		if not(isinstance(word, str)):
+			print("Error: First parameter must be a string!")
 
-def ft_remove_punctuation(string):
-	punctuation = "!#$%&'()\"*+,-./:;<=>?@[\\]^_`{|}~"
-	return(''.join([' ' if char in punctuation else char for char in string])) # List comprehention
-	# For each char in string test if char is in punctuation and if true change to <space> else keep char
 
 
-def ft_filter(string, number):
-	new_string = string.strip().split()
-	new_list = []
-	# new_list = [i if (len(i) > number) for i in new_string]
-
-	for i in new_string:
-		if (len(i) > number):
-			new_list.append(i)
-
-	print(new_list)
+	except:
+		print("Error: Unexpected number of arguments received!")
 
 if __name__ == "__main__":
-	string = sys.argv[1]
-	number = None
-
-	if not (isinstance(string, str)):
-		print("ERROR")
-	
-	if (not (isinstance(int(sys.argv[2]), int))):
-		print("ERROR")
-	number = int(sys.argv[2])
-
-	string = ft_remove_punctuation(string)
-	ft_filter(string, number)
+	ft_filterwords(sys.argv)
